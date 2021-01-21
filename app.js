d847898c211cfd61754ec1c2b31c24d8f0d4a761
Kou-kun42 <shinigamithekido@gmail.com>
2021-01-20 19:22:40 -0500

    added styles

--- app.js
+++ app.js
@@ -1,16 +1,20 @@
 // Require Libraries
 const express = require("express");
+require("dotenv").config();
 
 // Require tenorjs near the top of the file
 const Tenor = require("tenorjs").client({
   // Replace with your own key
-  Key: "TENOR_API_KEY", // https://tenor.com/developer/keyregistration
+  Key: process.env.TENOR_API_KEY, // https://tenor.com/developer/keyregistration
   Filter: "high", // "off", "low", "medium", "high", not case sensitive
   Locale: "en_US", // Your locale here, case-sensitivity depends on input
 });
 
 // App Setup
 const app = express();
+
+// Somewhere near the top
+app.use(express.static("public"));
 
 // Middleware
 const exphbs = require("express-handlebars");
@@ -20,9 +24,20 @@ app.set("view engine", "handlebars");
 
 // Routes
 app.get("/", (req, res) => {
-  console.log(req.query); // => "{ term: hey" }
-
-  res.render("home");
+  // Handle the home page when we haven't queried yet
+  term = "";
+  if (req.query.term) {
+    term = req.query.term;
+  }
+  // Tenor.search.Query("SEARCH KEYWORD HERE", "LIMIT HERE")
+  Tenor.Search.Query(term, "10")
+    .then((response) => {
+      // store the gifs we get back from the search
+      const gifs = response;
+      // pass the gifs as an object into the home page
+      res.render("home", { gifs });
+    })
+    .catch(console.error);
 });
 
 app.get("/greetings/:name", (req, res) => {
